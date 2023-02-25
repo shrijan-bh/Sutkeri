c01f201dd182dda816dc44545a42cf9d466060c7
Shrijan <shrijanb85@gmail.com>
2023-02-25 21:25:03 +0545

    changes in allow host

--- sutkeri/settings.py
+++ sutkeri/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-2*i^raa5emv7r6jeth855(by*-kre^(jrc8r282cpaskq+fuwn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
